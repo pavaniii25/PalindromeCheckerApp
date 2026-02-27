eba83915ae17f699d22b5d01b7c8383359e0f965
whospavaniii25 <pavanisowmyaa6@gmail.com>
2026-02-27 14:40:19 +0530

    Welcome Message

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+         /** @version 1.0
+             @author
+
+         **/
+                System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version:1.0");
+
+        System.out.println("System initialized successfully");
+
+    }
+}
