586454c30199675a1c143553301bf79a98c95553
whospavaniii25 <pavanisowmyaa6@gmail.com>
2026-03-09 14:40:23 +0530

    UC9

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,26 +1,33 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
 
     public static void main(String[] args) {
 
 
-        Stack<Character> stack = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input string: ");
+        String str = sc.nextLine();
 
-        for (char c : input.toCharArray()) {
-            stack.push(c);
+        boolean result = isPalindrome(str, 0, str.length() - 1);
+
+        System.out.println("Is Palindrome: " + result);
+
+        sc.close();
+    }
+
+    // Recursive function
+    private static boolean isPalindrome(String str, int start, int end) {
+
+        if (start >= end) {
+            return true;
         }
 
-        boolean isPalindrome = true;
-
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                isPalindrome = false;
-                break;
-            }
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
         }
-
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        return isPalindrome(str, start + 1, end - 1);
     }
 }
 
