a161d784ad73aed0bd22846fe06fe6d7931bd20c
whospavaniii25 <pavanisowmyaa6@gmail.com>
2026-03-09 15:00:49 +0530

    UC10

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,28 +1,38 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        String input = sc.nextLine();
 
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
+        // Normalize the string (remove spaces & punctuation, make lowercase)
+        String normalized = input.toLowerCase().replaceAll("[^a-z0-9]", "");
 
         boolean isPalindrome = true;
 
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
                 isPalindrome = false;
                 break;
             }
+            start++;
+            end--;
         }
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome:" + isPalindrome);
     }
 }
+
+
+
+
 
 
 
